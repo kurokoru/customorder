b42f98f9d53596113d9477e190f8d5aaa66600a4
mnhafid <hapidznur@gmail.com>
2025-06-24 18:45:49 +0700

    change icon

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -67,8 +67,8 @@ const RootLayout = ({ children }: RootLayoutProps) => {
           <div className="flex h-full max-h-screen flex-col gap-2">
             <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
               <Link href="/" className="flex items-center gap-2 font-semibold">
-                <TriangleAlert className="h-6 w-6" />
-                <span className="">{storeName} Inc</span>
+                {/* <TriangleAlert className="h-6 w-6" /> */}
+                <span className="">Pesona Indah Rooms & Restaurant</span>
               </Link>
             </div>
             <Navbar />
